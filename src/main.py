8b7184323932d14089948439c5a1572cc78300be
Txl3r <Harvey.Tyler@live.jobcorps.org>
2022-05-06 16:00:56 +0000

    made changes

--- src/main.py
+++ src/main.py
@@ -9,16 +9,24 @@ from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
 from models import db, User
+from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required, JWTManager
 #from models import Person
+
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_CONNECTION_STRING')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+app.config["JWT_SECRET_KET"] = "super-secret" 
+jwt = JWTManager(app)
+
 MIGRATE = Migrate(app, db)
 db.init_app(app)
 CORS(app)
 setup_admin(app)
+
+
 
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
@@ -30,14 +38,48 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
+
+@app.route("/login", methods={"POST"})
+def login():
+    username = request.json.get("username", None)
+    password = request.json.get("password", None)
+    if username != "test" or password != "test":
+        return jsonify({"msg": "Bad username or password"}), 401
+
+    access_token = create_access_token(identity=username)
+    return jsonify(access_token=access_token)
+
+@app.route("/protected", methods={"GET"})
+@jwt_required()
+def protected():
+    current_user = get_jwt_identity()
+    return jsonify(logged_in=current_user), 200
+
+
 @app.route('/user', methods=['GET'])
 def handle_hello():
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+    users = User.query.all()
+    all_users = list(map(lambda x: x.serialize(), users)) 
 
-    return jsonify(response_body), 200
+    
+
+    return jsonify(all_users), 200
+
+@app.route('/user/<int:user_id>', methods={'GET'})
+def each_user(user_id):
+
+    user = User.query.get(user_id)
+
+    return jsonify(user.email), 200
+
+@app.route('/user', methods=['POST'])
+def user():
+    request_body = request.get_json()
+    user = User(email=request_body['email'], password=request_body['password'], is_active=request_body['is_active'])
+    db.session.add(user)
+    db.session.commit()
+    return f"The new user {request_body['email']} was created successfully", 200
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
